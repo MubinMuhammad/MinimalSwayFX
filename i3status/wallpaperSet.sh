2902db076c0410edad765be9852957e878d705af
Mubin Muhammad <bssmibnkamal@gmail.com>
2024-02-11 16:19:15 +0600

    Next Commit

---
+++ i3status/wallpaperSet.sh
@@ -0,0 +1,8 @@
+n=`shuf -i 1-12 -n 1`
+
+swww img ~/.config/wallpapers/$n.jpg \
+  --transition-angle 225             \
+  --transition-type grow             \
+  --transition-step 10               \
+  --transition-pos 1.0,1.0           \
+  --transition-fps 60
