ba7458522c17e59304d818402042561d338c032d
MubinMuhammad <bssmibnkamal@gmail.com>
2024-04-06 10:07:45 +0600

    Fixed install.sh & updated sway/config

--- install.sh
+++ install.sh
@@ -8,6 +8,15 @@ configDir=$HOME/.config
 
 printf "\n------Installation Begins------\n\n"
 
+# if we already see $config_bak
+# in the $HOME/config, we just
+# want to delete it at this point.
+for config in "${configArray[@]}"; do
+  if [ -d "$configDir/${config}_bak" ]; then
+    rm -fr $configDir/${config}_bak
+  fi
+done
+
 # if any of the above mentioned
 # config exists in teh system,
 # move the config to a $config_bak
