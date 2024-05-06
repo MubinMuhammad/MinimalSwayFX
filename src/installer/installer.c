5564065225c140ca7a627a6fdee07c91e2c6f988
MubinMuhammad <bssmibnkamal@gmail.com>
2024-05-06 22:44:48 +0600

    It's going rough

--- src/installer/installer.c
+++ src/installer/installer.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void get_option(const char *in, char *out, int out_size, char *out_extension, int out_extension_size) {
+int get_option(const char *in, int in_size, 
+               char *out_forward, int out_forward_size,
+               char *out, int out_size, 
+               char *out_extension, int out_extension_size) {
   int out_idx = 0;
   int out_extension_idx = 0;
+  int idx = -1;
 
-  for (int i = 0; i < strlen(in); i++) {
+  for (int i = 0; i < in_size; i++) {
+    if (in[i] == '~') {
+      idx = i + 1;
+      break;
+    }
+    else {
+      out_forward[i] = in[i];
+    }
+  }
+
+  if (idx == -1) return 0;
+
+  for (int i = idx; i < in_size; i++) {
     if (((in[i] >= 'A' && in[i] <= 'Z') || in[i] == '_') && out_size >= out_idx - 1) {
       out[out_idx++] = in[i];
     }
@@ -17,10 +33,24 @@ void get_option(const char *in, char *out, int out_size, char *out_extension, in
   
   out[out_idx] = '\0';
   out_extension[out_extension_idx] = '\0';
+
+  return 1;
 }
 
 void text_to_option(const char *in, char *out_appended, int out_appended_size) {
-  
+  int in_size = strlen(in);
+  char forward_buffer[64], buffer[64], extension_buffer[64];
+
+  forward_buffer[0] = '\0';
+  buffer[0] = '\0';
+  extension_buffer[0] = '\0';
+
+  get_option(
+    in, in_size, 
+    forward_buffer, sizeof(forward_buffer), 
+    buffer, sizeof(buffer), 
+    extension_buffer, sizeof(extension_buffer)
+  );
 }
 
 void format_file(FILE *in, FILE *out) {
@@ -31,20 +61,23 @@ void format_file(FILE *in, FILE *out) {
 
   while (fgets(line_buffer, sizeof(line_buffer), in) != NULL) {
     tmp = strdup(line_buffer);
-    while ((token = strsep(&tmp, " \"")) != NULL) {
-      if (token[0] == '\0')
-        text_to_option(token + 1, write_buffer, sizeof(write_buffer));
-      else
-        strncat(write_buffer, line_buffer, sizeof(write_buffer) - strlen(line_buffer) + 1);
+    while ((token = strsep(&tmp, " ")) != NULL) {
+      text_to_option(token, write_buffer, sizeof(write_buffer));
     }
 
     fputs(write_buffer, out);
     write_buffer[0] = '\0';
+    free(tmp);
   }
 }
 
 int main() {
-  
+  FILE *fp = fopen("in.txt", "r");
+  FILE *wfp = fopen("out.txt", "w");
 
+  format_file(fp, wfp);
+
+  fclose(fp);
+  fclose(wfp);
   return 0;
 }
